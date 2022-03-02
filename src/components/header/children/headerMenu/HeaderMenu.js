9ec5cd947b84709f08307672253ef8d2ac0d6ec8
LittleRangiferTarandus <1422755729@qq.com>
2022-03-03 00:28:22 +0800

    navbar debug

--- src/components/header/children/headerMenu/HeaderMenu.js
+++ src/components/header/children/headerMenu/HeaderMenu.js
@@ -4,16 +4,29 @@ import CSS from "./HeaderMenu.module.css"
 import { useNavigate } from "react-router";
 import {BankTwoTone,ToolTwoTone,SmileTwoTone,EditTwoTone}from"@ant-design/icons"
 import store from '../../../../store/store';
+import {useLocation} from 'react-router-dom'
+import { useEffect } from 'react';
+import { useState } from 'react';
 function HeaderMenu(props) {
   const navigate = useNavigate()
+  const [selectKey,setSelectKye] = useState("1")
+  const location = useLocation()
+  useEffect(()=>{
+    let key = location.pathname.split("/")[1]
+    if(!key){
+      setSelectKye("home")
+    }else{
+      setSelectKye(key)
+    }
+  },[location])
   return (
     <div>
-      <Menu className={CSS.menu} theme="light" mode="horizontal" defaultSelectedKeys={['1']}>
-        <Menu.Item key="1" onClick={()=>{navigate("/home")}}>首页<BankTwoTone /></Menu.Item>
-        <Menu.Item key="3" onClick={()=>{navigate("/skill")}}>技术<ToolTwoTone /></Menu.Item>
-        <Menu.Item key="4" onClick={()=>{navigate("/mood")}}>随心贴<SmileTwoTone /></Menu.Item>
-        <Menu.Item key="5" onClick={()=>{navigate("/profile")}} style={{visibility:store.getState()?"visible":"hidden"}}>我的<EditTwoTone /></Menu.Item>
-        <Menu.Item key="6" onClick={()=>{navigate("/editor")}} style={{visibility:store.getState()?"visible":"hidden"}}>发帖<EditTwoTone /></Menu.Item>
+      <Menu className={CSS.menu} theme="light" selectedKeys={[selectKey]} mode="horizontal" defaultSelectedKeys={['home']}>
+        <Menu.Item key="home" onClick={()=>{navigate("/home")}}>首页<BankTwoTone /></Menu.Item>
+        <Menu.Item key="skill" onClick={()=>{navigate("/skill")}}>技术<ToolTwoTone /></Menu.Item>
+        <Menu.Item key="mood" onClick={()=>{navigate("/mood")}}>随心贴<SmileTwoTone /></Menu.Item>
+        <Menu.Item key="profile" onClick={()=>{navigate("/profile")}} style={{visibility:store.getState()?"visible":"hidden"}}>我的<EditTwoTone /></Menu.Item>
+        <Menu.Item key="editor" onClick={()=>{navigate("/editor")}} style={{visibility:store.getState()?"visible":"hidden"}}>发帖/编辑<EditTwoTone /></Menu.Item>
       </Menu>
     </div>
   )
